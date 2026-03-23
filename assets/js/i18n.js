b6dfd6d8005426508cb5f56e7d2ec776a5c124e5
Hatem Ali <hatemali716@gmail.com>
2026-03-23 23:26:51 +0200

    add service details page

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,9 +1,45 @@
 let translations = {};
-
+let currentLang = "en";
+let currentService = "financial";
 async function loadTranslations(lang) {
   const response = await fetch(`lang/${lang}.json`);
   translations = await response.json();
 }
+
+// 🔥 Render function
+function renderService(serviceKey) {
+  console.log(serviceKey)
+  const data = translations[serviceKey];
+  console.log(data);
+  document.getElementById("service-title").textContent = data.title;
+  document.getElementById("service-desc").textContent = data.desc;
+
+  const list = document.getElementById("service-list");
+  list.innerHTML = "";
+
+  data.items.forEach(item => {
+    const li = document.createElement("li");
+    li.innerHTML = `<i class="bi bi-check-circle"></i> ${item}`;
+    list.appendChild(li);
+  });
+}
+
+
+// 🔥 Click handling
+const links = document.querySelectorAll(".services-list a");
+
+links.forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    links.forEach(l => l.classList.remove("active"));
+    this.classList.add("active");
+
+    currentService = this.getAttribute("data-service");
+    renderService(currentService);
+  });
+});
+
 
 function applyTranslations() {
   document.querySelectorAll("[data-i18n]").forEach(el => {
@@ -17,6 +53,7 @@ async function changeLang(lang) {
 
   await loadTranslations(lang);
   applyTranslations();
+  renderService("financial");
 
  // Update dropdown label with flag
   document.getElementById("current-lang").innerHTML =
